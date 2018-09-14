6b0dda05be704a5a7ac65c6643f9c91057d50c81
savoptik <sav70792@gmail.com>
2018-09-14 19:19:19 +0300

    Описаны реализации функции раста на видеокарте и запуск расчёта.

---
+++ mirroring_image/resursis/kernel.c
@@ -0,0 +1,15 @@
+__kernel void square(__global unsigned char* input, int h, int v)
+{
+  int i = get_global_id(0);
+    int currentRow = i/(v/2);
+    int currentCol = i%(v/2);
+    unsigned char timer = input[(currentRow*v+currentCol)*3];
+    unsigned char timeg = input[(currentRow*v+currentCol)*3+1];
+    unsigned char timeb = input[(currentRow*v+currentCol)*3+2];
+    input[(currentRow*v+currentCol)*3] = input[(currentRow+(v-1)-currentCol)*3];
+    input[(currentRow*v+currentCol)*3+1] = input[(currentRow+(v-1)-currentCol)*3+1];
+    input[(currentRow*v+currentCol)*3+2] = input[(currentRow+(v-1)-currentCol)*3+2];
+    input[(currentRow+(v-1)-currentCol)*3] = timer;
+    input[(currentRow+(v-1)-currentCol)*3+1] = timeg;
+    input[(currentRow+(v-1)-currentCol)*3+2] = timeb;
+}
