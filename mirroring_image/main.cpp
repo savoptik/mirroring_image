659ce7cebc23a91f08ae7f94fe35d22c88289eac
savoptik <sav70792@gmail.com>
2018-09-14 17:59:30 +0300

    Initial Commit

---
+++ mirroring_image/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mirroring_image
+//
+//  Created by Артём Семёнов on 14/09/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
